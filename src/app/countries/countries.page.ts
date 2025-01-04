170ab629dede11274d0c46c890a07687c7957f2f
carlolrac <carlo13@gmx.com>
2025-01-04 05:48:33 +0000

    first draft of weatherpage - waiting for API key activation

--- src/app/countries/countries.page.ts
+++ src/app/countries/countries.page.ts
@@ -1,11 +1,11 @@
-import { Component, ViewChild,OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { IonContent, IonHeader, IonTitle, IonToolbar, IonButtons, IonBackButton, IonImg, IonCardTitle, IonCard, IonCardHeader, IonCardSubtitle, IonCardContent, IonButton, IonInput } from '@ionic/angular/standalone';
 //import { ActivatedRoute } from '@angular/router';
 import { MyUnitService } from '../services/my-unit.service';
 import { MyHttpServiceService } from '../services/my-http-service.service';
-import { CapacitorHttp, HttpResponse } from '@capacitor/core';
+//import { CapacitorHttp, HttpResponse } from '@capacitor/core';
 import { HttpOptions } from '@capacitor/core';
 import { RouterLink } from '@angular/router';
 //import { }
@@ -35,7 +35,7 @@ export class CountriesPage implements OnInit {
 
   hidden:boolean = true;
 
-  @ViewChild('myCCA2_1') input!: IonInput;
+ // @ViewChild('myCCA2_1') input!: IonInput;
 
   options: HttpOptions = {
     url: 'https://restcountries.com/v3.1/name/'
@@ -105,13 +105,26 @@ export class CountriesPage implements OnInit {
     this.mus.set("country", this.cca2s[index]);
     this.mus.set("countryName", this.countryNames[index]);
 
-    this.mus.set("capital", this.capitals[index]);
-    this.mus.set("latlng", this.capitalsLatLng[index]);
+    //this.mus.set("capital", this.capitals[index]);
+    //this.mus.set("latlng", this.capitalsLatLng[index]);
 
     //this.countries = await this.mhs.get(this.options);
     console.log(index);
     //console.log(this.input.value);
     console.log(this.cca2s[index]);
+  }
+
+  setCapital(index: any) {
+    //this.mus.set("country", this.cca2s[index]);
+    //this.mus.set("countryName", this.countryNames[index]);
+
+    this.mus.set("capital", this.capitals[index]);
+    this.mus.set("latlng", this.capitalsLatLng[index]);
+
+    //this.countries = await this.mhs.get(this.options);
+    //console.log(index);
+    //console.log(this.input.value);
+    console.log(this.capitals[index]);
   }
 
 
--- src/app/weather/weather.page.ts
+++ src/app/weather/weather.page.ts
@@ -1,20 +1,98 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonButton, IonCard, IonImg, IonCardTitle, IonCardSubtitle, IonCardContent, IonCardHeader, IonButtons, IonBackButton } from '@ionic/angular/standalone';
+
+import { MyUnitService } from '../services/my-unit.service';
+import { MyHttpServiceService } from '../services/my-http-service.service';
+import { HttpOptions } from '@capacitor/core';
+//import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-weather',
   templateUrl: './weather.page.html',
   styleUrls: ['./weather.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [IonBackButton, IonButtons, IonCardHeader, IonCardContent, IonCardSubtitle, IonCardTitle, IonImg, IonCard, IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule ]
 })
+
 export class WeatherPage implements OnInit {
 
-  constructor() { }
+  climate!: any;
+  api:String = "c618681616432ab48624042bca68eeb6";
+              //c618681616432ab48624042bca68eeb6
+  capital!:any;
+  capitalLat!:any;
+  capitalLng!:any;
+  unit!:String;
 
-  ngOnInit() {
+  options: HttpOptions = {
+    url: 'https://api.openweathermap.org/data/2.5/weather?lat=',
   }
 
-}
+      // LATITUDE 
+    // &lon=LONGITUDE&units=UNITS&appid=YOUR_API_KEY '
+
+
+  constructor(private mus:MyUnitService, private mhs:MyHttpServiceService) {}
+
+  ngOnInit() {
+    let timeInMs = 500;
+    let timeout= setTimeout( () => {
+      this.getCapitalData();
+    }, timeInMs );
+  }
+
+  async getCapitalData() {
+      try {
+        this.capital = await this.mus.get("capital");
+        console.log(this.capital);
+        let tmpLatLng = await this.mus.get("latlng");
+        console.log(tmpLatLng);
+        this.capitalLat = tmpLatLng[0];
+        this.capitalLng = tmpLatLng[1];
+        this.unit = await this.mus.get("unit");
+        console.log(this.unit);
+      } catch (error) {
+        console.log(error);
+      }
+  
+      if (!this.unit) {
+        this.unit = "metric";
+      }
+
+      this.options.url = this.options.url + this.capitalLat;
+      this.options.url = this.options.url + "&lon=" + this.capitalLng;
+      this.options.url = this.options.url + "&units=" + this.unit;
+      this.options.url = this.options.url + "&appid" + this.api;
+      console.log(this.options.url);
+  
+      this.getWeather();
+  
+      /*
+      if (this.unit === null) {
+        this.selUnit = "metric";
+        this.mus.set("unit","metric");
+      } else {
+        this.selUnit = this.unit;
+      }
+      */
+    }
+
+    async getWeather() {
+      this.climate = await this.mhs.get(this.options);
+  
+    //  this.countries.forEach((country:any) => {
+    //    console.log(country.cca2);
+    //    this.cca2s.push(country.cca2);
+    //    this.countryNames.push(country.name.official);
+    //    this.capitals.push(country.capital);
+    //    this.capitalsLatLng.push(country.latlng);
+    //  });
+    
+  
+      console.log(JSON.stringify(this.climate));
+    }
+  }
+
+
